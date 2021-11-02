1ee4600215a6b93a7a46540a2a7bda070fc82d7d
Yau Kuen Tsang <danielyktsang@gmail.com>
2021-11-01 18:12:53 -0700

    Installed Chi and using Chi for routing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module lenlocked
 
 go 1.17
+
+require github.com/go-chi/chi/v5 v5.0.5 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html; charset=utf-8")
-  fmt.Fprint(w, "<h1>Welcome to my wow site!</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my wow site!</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html; charset=utf-8")
-  fmt.Fprint(w, "<h1>Contact Page</h1>To get in touch, email me at <a href=\"mailto:daniel7288@gmail.com\">daniel7288@gmail.com</a>.")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1>To get in touch, email me at <a href=\"mailto:daniel7288@gmail.com\">daniel7288@gmail.com</a>.")
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html; charset=utf-8")
-  fmt.Fprint(w, `<h1>FAQ Page</h1>
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
 <ul>
   <li><b>Q: Is there a free version?</b>
       <br>A: Yes!! we offer a free trial for 30 days on any paid plans.</br></li>
@@ -41,29 +42,36 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 //   }
 // }
 
-type Router struct {}
+type Router struct{}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  switch r.URL.Path {
-  case "/":
-    homeHandler(w, r)
-  case "/contact":
-    contactHandler(w, r)
-  case "/faq":
-    faqHandler(w, r)
-  default:
-    // TODO: handle the page not found error
-    http.Error(w, "Page not found", http.StatusNotFound)
-    // w.WriteHeader(http.StatusNotFound)
-    // fmt.Fprint(w, "Page not found")
-  }
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		// TODO: handle the page not found error
+		http.Error(w, "Page not found", http.StatusNotFound)
+		// w.WriteHeader(http.StatusNotFound)
+		// fmt.Fprint(w, "Page not found")
+	}
 }
 
 func main() {
-  // http.HandleFunc("/", pathHandler)
-  // http.HandleFunc("/contact", contactHandler)
-  // var router http.HandlerFunc = pathHandler
-  var router Router
-  fmt.Println("Starting the server on :3000...")
-  http.ListenAndServe(":3000", router)
+	// http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
+	// var router http.HandlerFunc = pathHandler
+	// var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
 }
